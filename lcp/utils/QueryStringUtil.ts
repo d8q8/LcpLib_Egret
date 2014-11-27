88a7e3c9c0e7ec4aa5c4b15207385c180b4fb118
d8q8 <d8q8@163.com>
2014-11-28 03:30:28 +0800

    查询字符串类

---
+++ lcp/utils/QueryStringUtil.ts
@@ -0,0 +1,78 @@
+/**
+ * Created by d8q8 on 2014/11/28.
+ * @module lcp
+ * @class QueryStringUtil
+ * @constructor
+ **/
+module lcp {
+    export class QueryStringUtil {
+        public CLASS_NAME:string = "QueryStringUtil";
+
+        public static _query:string;
+        public static _hasRequested:boolean;
+        public static _pairMap:any;
+
+        /**
+         * 获取浏览器URL的键值对
+         * @returns {string}
+         */
+        public static get queryString():string {
+            if (!this._hasRequested) {
+                this._hasRequested = true;
+
+                if (egret.ExternalInterface) {
+                    try {
+                        var query:string = egret.ExternalInterface.call('document.location.search.toString');
+
+                        if (query != '' && query != null) {
+                            this._query = query.substring(1);
+
+                            var pairs:Array<any> = <Array<any>>this._query.split('&');
+                            var i:number         = -1;
+                            var pair:Array<any> = [];
+
+                            this._pairMap = {};
+
+                            while (++i < pairs.length) {
+                                pair = pairs[i].split('=');
+                                this._pairMap[pair[0]] = pair[1];
+                            }
+                        }
+                    } catch (e) {}
+                }
+            }
+
+            return this._query;
+        }
+
+        /**
+         * 从键中返回一个值
+         * @param key
+         * @returns {*}
+         */
+        public static getValue(key:string):string {
+            if (this.queryString == null)
+                return null;
+
+            return this._pairMap[key];
+        }
+
+        /**
+         * 检测键是否存在
+         * @param key
+         * @returns {boolean}
+         */
+        public static hasKey(key:string):boolean {
+            return this.getValue(key) ? true : false;
+        }
+
+        /**
+         * 类名
+         * @returns {string}
+         */
+        public toString():string {
+            //console.log("ClassName",this.CLASS_NAME);
+            return this.CLASS_NAME;
+        }
+    }
+}
